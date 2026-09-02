16204b65cac56f55f822246f3043157f08d02d4e
AnataVortex7 <akshaymerat@gmail.com>
2026-09-02 11:32:28 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -11,7 +11,7 @@ else
     echo "Sync will not work without Google Drive config."
 fi
 
-REMOTE_NAME="gdrive1"
+REMOTE_NAME="gdrive"
 if [ -f /root/.config/rclone/rclone.conf ]; then
     EXTRACTED_NAME=$(grep -o '^\[.*\]$' /root/.config/rclone/rclone.conf | head -n 1 | tr -d '[]')
     if [ -n "$EXTRACTED_NAME" ]; then
